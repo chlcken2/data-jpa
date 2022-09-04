fcb639bf5f2576499a8dc9ac37f74ebb8fb50fe6
chlcken2 <ckdgus988@naver.com>
2022-09-04 13:35:46 +0900

    [feature] dto 반환을 위한 jpql에서의 new 오퍼레이션 사용 방법

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
 
 import java.util.List;
 
@@ -17,6 +19,8 @@ class MemberRepositoryTest {
 
     @Autowired
     MemberRepository memberRepository;
+    @Autowired
+    TeamRepository teamRepository;
 
     @Test
     public void basicCRUD() {
@@ -45,15 +49,31 @@ class MemberRepositoryTest {
     }
 
     @Test
-    public void testQuery() {
+    public void findUsernameList() {
         Member m1 = new Member("aaa", 10);
         Member m2 = new Member("bbbb", 20);
 
         memberRepository.save(m1);
         memberRepository.save(m2);
 
-        List<Member> result = memberRepository.findUser("aaa", 10);
-        Assertions.assertThat(result.get(0)).isEqualTo(m1);
+        List<String> usernameList = memberRepository.findUsernameList();
+        for (String s : usernameList) {
+            System.out.println("s = " + s);
 
+        }
+    }
+    @Test
+    public void findMemberDto() {
+        Team team = new Team("teamA");
+        teamRepository.save(team);
+
+        Member m1 = new Member("aaa", 10);
+        m1.setTeam(team);
+        memberRepository.save(m1);
+
+        List<MemberDto> memberDto = memberRepository.findMemberDto();
+        for (MemberDto dto : memberDto) {
+            System.out.println("dto = " + dto);
+        }
     }
 }
